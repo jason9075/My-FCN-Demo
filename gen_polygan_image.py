c1d422d91210dc38440159bc9ba0b1521983fef5
jason9075 <jason9075@gmail.com>
2018-10-13 22:16:15 +0800

    fix bug: missing mask images

--- gen_polygan_image.py
+++ gen_polygan_image.py
@@ -22,21 +22,20 @@ for imageFile in os.listdir('data/'):
   tree = ET.parse('label/' + name + '.xml')
   polygon = tree.find('object/polygon')
   
-  #該圖沒有polygon
-  if(polygon==None):
-    continue
-  
-  coord_list = []
-  for i in range(1, int(len(polygon.getchildren())/2 + 1)):
-    x = polygon.find('x%d' % i).text
-    y = polygon.find('y%d' % i).text
-    coord_list.append((int(x),int(y)))
-  
   mask = Image.new('RGBA', im.size)
-  d = ImageDraw.Draw(mask)
-  d.polygon(tuple(coord_list), outline='#000', fill='#0F0')
+  #該圖有polygon
+  if(polygon!=None):
+    coord_list = []
+    for i in range(1, int(len(polygon.getchildren())/2 + 1)):
+      x = polygon.find('x%d' % i).text
+      y = polygon.find('y%d' % i).text
+      coord_list.append((int(x),int(y)))
+    
+    d = ImageDraw.Draw(mask)
+    d.polygon(tuple(coord_list), outline='#000', fill='#0F0')
 
   mask.save('mask/'+name+'.png')
+  print("save:"+name)
           
 #out = Image.new('RGBA', im.size)
 #out.paste(im, (0, 0), mask)
